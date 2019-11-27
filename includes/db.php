3b379a68fc1580de4398ff9c7c4fdc316c73c75e
VZobbe <vicc66@live.dk>
2019-11-27 11:11:50 +0100

    Making files reusable
    putting files that need to be reused into an includes folder, so that they can be used agian

---
+++ includes/db.php
@@ -0,0 +1,23 @@
+<?php
+// all the db login values are put into an array called $db, im using root as password because im on a mac.
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "root";
+$db['db_name'] = "cms";
+
+// using a loop and converting the key value pairs of the $db array into constants and uppercase the key 
+foreach( $db as $key => $value ) {
+
+define(strtoupper($key), $value);
+
+
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connection) {
+
+echo "the db is connected";
+
+
+}
