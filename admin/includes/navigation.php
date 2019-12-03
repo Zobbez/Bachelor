166401e05db972fd1cad2bcf9f5fd002d2f67aa4
VZobbe <vicc66@live.dk>
2019-12-03 11:44:58 +0100

    Start of posts

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -41,7 +41,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#postsDropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="postsDropdown" class="collapse">
                             <li>
-                                <a href="#">View all posts</a>
+                                <a href="./posts.php">View all posts</a>
                             </li>
                             <li>
                                 <a href="#">Add post</a>
@@ -50,7 +50,7 @@
                     </li>
 
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Catagories </a>
+                        <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Catagories </a>
                     </li>
               
                     <li class="active">
